0953804ae16cd41f7fc2b4d40b30cbcb43d07aa9
keita sato home <info.kt310@gmail.com>
2021-02-11 14:39:42 +0900

    ルーティング削除

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ use App\Http\Controllers\Api\RankinghalftermController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/ranking', [RankinghalftermController::class, 'index'])->name('index');
